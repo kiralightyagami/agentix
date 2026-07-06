08e20c5225d43fec1072fe3e3e46a8a868cec6b9
kiralightyagami <crucisgamer98@gmail.com>
2026-07-06 14:58:06 +0530

    theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
+
 import type { Metadata } from "next";
 import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const manrope = Manrope({
   variable: "--font-manrope",
   subsets: ["latin"],
-  weight: ["400","500","600","700"]
+  weight: ["400", "500", "600", "700"],
 });
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-  weight: ["400","500","600","700"]
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,11 +27,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${manrope.variable} ${inter.variable} h-full antialiased bg-background text-foreground`}
-    >
-      <body className="">{children}</body>
+    <html lang="en">
+      <body className={`${manrope.variable} ${inter.variable} antialiased `}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <main className="bg-background text-foreground">{children}</main>
+      </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen font-manrope">
-    hi there
+    <div className="min-h-screen font-display">
+    <ModeToggle/>
+    <p className="font-inter">
+      Deploy Agents in minutes 
+    </p>
     </div>
   );
 }
